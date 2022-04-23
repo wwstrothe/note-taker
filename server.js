76800639790a6d2a161f0b3ab27e9d0325c97b11
wwstrothe <95983821+wwstrothe@users.noreply.github.com>
2022-04-23 15:08:53 -0700

    set paths to serve html and css styles

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
+const fs = require('fs');
+const path = require('path')
+
 const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.use(express.json());
+app.use(express.static("public"));
+
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
 app.listen(port, () => {
